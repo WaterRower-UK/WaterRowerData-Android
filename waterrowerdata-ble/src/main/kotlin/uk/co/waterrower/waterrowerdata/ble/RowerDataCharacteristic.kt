4065701966347c639851d0ea2e9622d0ab27f855
Niek Haarman <niek@305.nl>
2020-07-17 15:04:28 +0200

    Decode average stroke rate

--- waterrowerdata-ble/src/main/kotlin/uk/co/waterrower/waterrowerdata/ble/RowerDataCharacteristic.kt
+++ waterrowerdata-ble/src/main/kotlin/uk/co/waterrower/waterrowerdata/ble/RowerDataCharacteristic.kt
@@ -1,8 +1,23 @@
 package uk.co.waterrower.waterrowerdata.ble
 
+import kotlin.experimental.and
+
 object RowerDataCharacteristic {
 
-    fun decode(bytes: ByteArray): Any? {
+    fun decode(bytes: ByteArray): RowerData {
+        return RowerData(
+            averageStrokeRate = averageStrokeRate(bytes)
+        )
+    }
+
+    private fun averageStrokeRate(data: ByteArray): Double? {
+        val flagsValue = data[0]
+        val averageStrokeRatePresent = flagsValue.and(0b10).toInt() != 0
+
+        if (averageStrokeRatePresent) {
+            return data[2] / 2.0
+        }
+
         return null
     }
 }
--- waterrowerdata-ble/src/test/kotlin/uk/co/waterrower/waterrowerdata/ble/RowerDataCharacteristicTest.kt
+++ waterrowerdata-ble/src/test/kotlin/uk/co/waterrower/waterrowerdata/ble/RowerDataCharacteristicTest.kt
@@ -1,16 +1,40 @@
 package uk.co.waterrower.waterrowerdata.ble
 
 import com.nhaarman.expect.expect
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import uk.co.waterrower.waterrowerdata.ble.testutil.CharacteristicData
+import uk.co.waterrower.waterrowerdata.ble.testutil.RowerDataCharacteristicFlags
 
 class RowerDataCharacteristicTest {
 
-    @Test
-    fun `encoding empty data returns null`() {
-        /* When */
-        val result = RowerDataCharacteristic.decode(byteArrayOf())
+    @Nested
+    inner class `Average Stroke Rate`() {
 
-        /* Then */
-        expect(result).toBeNull()
+        @Test
+        fun `averageStrokeRate not present results in null value`() {
+            /* Given */
+            val flags = RowerDataCharacteristicFlags.create(averageStrokeRatePresent = false)
+            val data = CharacteristicData.create(flags)
+
+            /* When */
+            val result = RowerDataCharacteristic.decode(data)
+
+            /* Then */
+            expect(result.averageStrokeRate).toBeNull()
+        }
+
+        @Test
+        fun `averageStrokeRate present results in uint8 value with binary exponent minus one`() {
+            /* Given */
+            val flags = RowerDataCharacteristicFlags.create(averageStrokeRatePresent = true)
+            val data = CharacteristicData.create(flags, 7)
+
+            /* When */
+            val result = RowerDataCharacteristic.decode(data)
+
+            /* Then */
+            expect(result.averageStrokeRate).toBe(3.5)
+        }
     }
 }
